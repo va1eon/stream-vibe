fc0aaea2910f5125f05162ccabdbfe014e145df9
Valery Mikhailov <va1eonmix@gmail.com>
2025-12-01 13:08:38 +0700

    InputMaskCollection module is done

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import OverlayMenu from '@/modules/OverlayMenu'
 import SliderCollection from '@/modules/SliderCollection'
 import TabsCollection from '@/modules/TabsCollection'
 import VideoPlayerCollection from '@/modules/VideoPlayerCollection'
+import InputMaskCollection from '@/modules/InputMaskCollection'
 
 document.addEventListener('DOMContentLoaded', () => {
   new OverlayMenu()
   new SliderCollection()
   new TabsCollection()
   new VideoPlayerCollection()
+  new InputMaskCollection()
 })
