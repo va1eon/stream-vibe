20d9d2049ef360ba0aa0e3d01be67eb0dbf2fbd7
Valery Mikhailov <va1eonmix@gmail.com>
2025-12-01 08:28:51 +0700

    Tags component is added

---
+++ src/components/Tags/Tags.jsx
@@ -0,0 +1,19 @@
+import './Tags.scss'
+
+const Tags = (props) => {
+  const {
+    items = [],
+  } = props
+
+  return (
+    <div className="tags">
+      <ul className="tags__list">
+        {items.map((tag, index) => (
+          <li className="tags__item" key={index}>{tag}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Tags
