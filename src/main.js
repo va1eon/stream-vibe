d025ff783602fb30851eee6996f9f1f524a32b4a
Valery Mikhailov <va1eonmix@gmail.com>
2025-12-01 12:08:32 +0700

    show page is done

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import OverlayMenu from '@/modules/OverlayMenu'
 import SliderCollection from '@/modules/SliderCollection'
 import TabsCollection from '@/modules/TabsCollection'
+import VideoPlayerCollection from '@/modules/VideoPlayerCollection'
 
 document.addEventListener('DOMContentLoaded', () => {
   new OverlayMenu()
   new SliderCollection()
   new TabsCollection()
+  new VideoPlayerCollection()
 })
